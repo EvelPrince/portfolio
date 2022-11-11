47f4aaea8eb413c64150c09f966d913656024ba4
Evel <89169653+EvelPrince@users.noreply.github.com>
2022-11-10 21:23:35 -0600

    Add files via upload

---
+++ change.js
@@ -0,0 +1,23 @@
+function change01() {
+    document.getElementById("large").src = "images/bluesew.jpg";
+}
+
+function change02() {
+
+    document.getElementById("large").src = "images/gasstation.jpg";
+}
+
+function change03() {
+    document.getElementById("large").src = "images/jandG.jpg";
+}
+
+function change04() {
+    document.getElementById("large").src = "images/buff.jpg";
+}
+
+function change05() {
+    document.getElementById("large").src = "images/rain.jpg";
+}
+function change06() {
+    document.getElementById("large").src = "images/shakymoon.jpg";
+}
